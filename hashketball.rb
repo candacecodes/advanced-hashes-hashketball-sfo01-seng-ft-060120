ad00193009b0ee650fdb1f45dcdbd66169ecc048
eager-class-5814 <candacehchoe@gmail.com>
2020-05-22 00:31:30 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -133,7 +133,7 @@ def num_points_scored(player_n)
   points = nil
   game_hash.each do |home_away, keys|
     keys[:players].each do |player|
-      return player[:points] if player[:player_n] == player_name
+      return player[:points] if player[:player_name] == player_n
     end 
   end 
 end 
