d74181199615a2937bece06a93e2ada6e18e05cf
eager-class-5814 <candacehchoe@gmail.com>
2020-05-22 01:13:17 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -168,7 +168,7 @@ end
 
 def player_stats(player_n)
   game_hash.each do |home_away, keys|
-    keys[:players].each do |player|
+    keys[:player].each do |player|
       if players[:player_name] == player_n
         return[:player_stats]
       end
